8fec4d220161db7db11cbbadac560862d54d4234
Mohamed Kamboua <mkambouay@gmail.com>
2022-03-15 21:26:19 +0100

    + adding eureka server + adding eureka discovery client + configure item microservice and product microservice with eureka discovery

--- servicio-productos/src/main/java/com/springboot/app/servicio/productos/controllers/ProductoController.java
+++ servicio-productos/src/main/java/com/springboot/app/servicio/productos/controllers/ProductoController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.app.servicio.productos.models.entity.Producto;
 import com.springboot.app.servicio.productos.models.service.IProductoService;
+
 
 @RestController
 public class ProductoController {
