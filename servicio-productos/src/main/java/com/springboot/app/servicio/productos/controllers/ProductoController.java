3d8ce484734bc9d5c1bab5b7b0c2028baf225b4f
Mohamed Kamboua <mkambouay@gmail.com>
2022-03-19 19:02:37 +0100

    comentarios

--- servicio-productos/src/main/java/com/springboot/app/servicio/productos/controllers/ProductoController.java
+++ servicio-productos/src/main/java/com/springboot/app/servicio/productos/controllers/ProductoController.java
@@ -42,11 +42,11 @@ public class ProductoController {
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		// producto.setPort(port);
 
-		try {
-			Thread.sleep(2000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(2000L);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 
 		return producto;
 	}
